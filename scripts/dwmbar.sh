d2ea1eb274f85a88c1dec8264c2722483d764253
Ethan M. Lee <ethan@mlee.xyz>
2022-01-14 15:21:03 -0500

    force 100% if fully charged

--- scripts/dwmbar.sh
+++ scripts/dwmbar.sh
@@ -7,11 +7,14 @@ do
   TIME=$(cat /tmp/curTime.tmp)
   VOLUME=$(pamixer --get-volume-human)
   UPOWER=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state" | awk '{ print $NF }')
-  CHARGE=↓; [ "$UPOWER" = "charging" ] && STATE=↑; [ "$UPOWER" = "fully-charged" ] && unset CHARGE
-  BATTERY=$(cat /sys/class/power_supply/BAT?/capacity)%
+  CHARGE=↓; [ "$UPOWER" = "charging" ] && STATE=↑
+  CAPACITY=$(cat /sys/class/power_supply/BAT?/capacity)%
+  BATTERY=$CAPACITY$CHARGE
+  [ "$UPOWER" = "fully-charged" ] && BATTERY=100%
   MEMORY=$(free -h | grep 'Mem' | awk '{ print $3 }')
   CPU=$(sensors acpitz-acpi-0 -u | grep temp1_input | awk '{ print $2 }' | sed 's/\..*//g')C
 
-  xsetroot -name " vol:$VOLUME | bat:$BATTERY$CHARGE | mem:$MEMORY | temp:$CPU | $TIME"
+  xsetroot -name " vol:$VOLUME | bat:$BATTERY | mem:$MEMORY | temp:$CPU | $TIME"
+
   sleep 15s
 done
