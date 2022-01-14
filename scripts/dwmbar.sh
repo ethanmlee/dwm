a9ab47c92c7bb18d1fa542f9da4a04d3a54ead4d
Ethan M. Lee <ethan@mlee.xyz>
2022-01-14 14:53:09 -0500

    no arrow on fully charged

--- scripts/dwmbar.sh
+++ scripts/dwmbar.sh
@@ -7,11 +7,11 @@ do
   TIME=$(cat /tmp/curTime.tmp)
   VOLUME=$(pamixer --get-volume-human)
   UPOWER=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state" | awk '{ print $NF }')
-  STATE=↓; [ "$UPOWER" = "charging" ] && STATE=↑
+  CHARGE=↓; [ "$UPOWER" = "charging" ] && STATE=↑; [ "$UPOWER" = "fully-charged" ] && unset CHARGE
   BATTERY=$(cat /sys/class/power_supply/BAT?/capacity)%
   MEMORY=$(free -h | grep 'Mem' | awk '{ print $3 }')
   CPU=$(sensors acpitz-acpi-0 -u | grep temp1_input | awk '{ print $2 }' | sed 's/\..*//g')C
 
-  xsetroot -name " vol:$VOLUME | bat:$BATTERY$STATE | mem:$MEMORY | temp:$CPU | $TIME"
+  xsetroot -name " vol:$VOLUME | bat:$BATTERY$CHARGE | mem:$MEMORY | temp:$CPU | $TIME"
   sleep 15s
 done
