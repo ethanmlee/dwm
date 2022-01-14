7c5f307b9ab0837aaff46aa340e5948aa196566f
Ethan M. Lee <ethan@mlee.xyz>
2022-01-14 15:25:59 -0500

    readability/formating

--- scripts/dwmbar.sh
+++ scripts/dwmbar.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
-
-echo $$ > /tmp/dwmbarpid.tmp # pid in file for other scripts
+echo $$ > /tmp/dwmbarpid.tmp
 
 while :
 do
   TIME=$(cat /tmp/curTime.tmp)
   VOLUME=$(pamixer --get-volume-human)
   UPOWER=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state" | awk '{ print $NF }')
-  CHARGE=↓; [ "$UPOWER" = "charging" ] && STATE=↑
-  CAPACITY=$(cat /sys/class/power_supply/BAT?/capacity)%
-  BATTERY=$CAPACITY$CHARGE
-  [ "$UPOWER" = "fully-charged" ] && BATTERY=100%
+  CAPACITY=$(cat /sys/class/power_supply/BAT?/capacity)%; CHARGE=↓; [ "$UPOWER" = "charging" ] && STATE=↑
+  BATTERY=$CAPACITY$CHARGE; [ "$UPOWER" = "fully-charged" ] && BATTERY=100%
   MEMORY=$(free -h | grep 'Mem' | awk '{ print $3 }')
   CPU=$(sensors acpitz-acpi-0 -u | grep temp1_input | awk '{ print $2 }' | sed 's/\..*//g')C
 
