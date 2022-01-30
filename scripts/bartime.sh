249567799d89c53c4c4573b28af754a8c09d6d4b
Ethan M. Lee <ethan@mlee.xyz>
2022-01-29 23:33:42 -0500

    .scripts to path

--- scripts/bartime.sh
+++ scripts/bartime.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-#cd "${0%"/"*}" #sets relative path
-
 echo $(date '+%a %d.%m.%+4Y %I:%M %p %Z') > /tmp/curTime.tmp
-nohup ~/.scripts/dwmbar.sh > /dev/null & # start dwmbar.sh script
+nohup dwmbar.sh > /dev/null & # start dwmbar.sh script
 while :
 do
   sleep $((60 - $(date +%S) )) && # sleep until next full min
   echo $(date '+%a %d.%m.%+4Y %I:%M %p %Z') > /tmp/curTime.tmp # save current time format to /tmp/curTime.tmp
-  ~/.scripts/refreshbar.sh # refresh dwmbar after updating time in file
+  refreshbar.sh # refresh dwmbar after updating time in file
 done
