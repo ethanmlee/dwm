bec43cb9b93d4b4acd141261a5bc75b98d652f50
Ethan M. Lee <ethan@mlee.xyz>
2022-09-29 11:32:54 -0400

    clean static const rules

--- config.def.h
+++ config.def.h
@@ -37,9 +37,7 @@ static const Rule rules[] = {
 	/* class   instance     title           tags mask  isfloating   monitor */
 	{ "Gimp",  NULL,        NULL,           0,         1,           -1 },
 	{ "feh",   NULL,        NULL,           0,         1,           -1 },
-	{ NULL,    "urxvt",     NULL,           0,         0,           -1 },
 	{ NULL,    "nmtui",     NULL,           0,         1,           -1 },
-	{ NULL,    NULL,        "Event Tester", 0,         0,           -1 }, /* xev */
 	{ NULL,    "spterm",    NULL,           0,         1,           -1 },
 	{ NULL,    "calc",      NULL,           0,         1,           -1 },
 	{ NULL,    "keepassxc", NULL,           0,         1,           -1 },
