1f83edeedd7f17c8516413029ec7f41c7129e0e4
Ethan M. Lee <ethan@mlee.xyz>
2022-10-10 16:54:35 -0400

    vanity gaps

--- config.def.h
+++ config.def.h
@@ -6,7 +6,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int snap      = 20;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font Mono:style=Regular:size=10" };
@@ -95,6 +100,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_p,         resetlayout,    {0} },
 	{ MODKEY|ShiftMask,             XK_j,         movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,         movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_minus,     incrgaps,       {.i = -5 } },
+	{ MODKEY,                       XK_equal,     incrgaps,       {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_minus,     togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_equal,     defaultgaps,    {0} },
 	{ MODKEY,                       XK_Return,    zoom,           {0} },
 	{ MODKEY,                       XK_Tab,       view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,         killclient,     {0} },
