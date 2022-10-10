88fd238575139095c03f9415f12e1c3562e2ed07
Ethan M. Lee <ethan@mlee.xyz>
2022-10-10 18:53:13 -0400

    im on fire today

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 20;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Nerd Font Mono:style=Regular:size=10" };
@@ -27,14 +27,14 @@ static const char *colors[][4]      = {
 	[SchemeNorm]     = { "#000000",   "#000000",   col_gray,    col_gray  },
 	[SchemeSel]      = { "#000000",   "#000000",   col_magenta, col_cyan  },
 	[SchemeStatus]   = { col_gray,    col_black,   "#000000",   "#000000" },
-	[SchemeTagsSel]  = { col_black,   col_magenta, "#000000",   "#000000" },
+	[SchemeTagsSel]  = { col_black,   col_gray, "#000000",   "#000000" },
 	[SchemeTagsNorm] = { col_gray,    col_black,   "#000000",   "#000000" },
 	[SchemeInfoSel]  = { col_white,   col_black,   "#000000",   "#000000" },
 	[SchemeInfoNorm] = { col_white,   col_black,   "#000000",   "#000000" },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "i", "ii", "iii", "iv", "v", "vi", "vii", "iix", "ix" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -120,6 +120,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return,    zoom,           {0} },
 	{ MODKEY,                       XK_Tab,       view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,         killclient,     {0} },
+	{ MODKEY,                       XK_BackSpace, cyclelayout,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_BackSpace, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_y,         setlayout,      {.v = &layouts[14]} },
 	{ MODKEY,                       XK_u,         setlayout,      {.v = &layouts[1]} },
