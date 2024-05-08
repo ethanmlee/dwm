20a886834116de16af1d6500fd5364afef4e0f8f
Ethan M. Lee <ethan@mlee.xyz>
2024-05-08 01:20:56 -0400

    gimp doesn't need to be floating

--- config.def.h
+++ config.def.h
@@ -44,7 +44,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */  
 	/* class    instance    title                 tags mask  isfloating   monitor */
-	{ "Gimp",   NULL,       NULL,                 0,         1,           -1 },
 	{ NULL,    "nmtui",     NULL,                 0,         1,           -1 },
 	{ NULL,    "spterm",    NULL,                 0,         1,           -1 },
 	{ NULL,    "calc",      NULL,                 0,         1,           -1 },
